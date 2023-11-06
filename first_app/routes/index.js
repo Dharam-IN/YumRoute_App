10c3d13067ea4ff5c42511e698a4df41d38d9f9a
dharamraj-dev <dharamrajyadav6312@gmail.com>
2023-11-06 15:40:20 +0530

    Session and Cookies

--- first_app/routes/index.js
+++ first_app/routes/index.js
@@ -4,9 +4,43 @@ const userModel = require('./users')
 
 /* GET home page. */
 router.get("/", function(req,res){
+  res.cookie("age", 25)
   res.render('index')
 });
 
+router.get("/check", function(req, res){
+  console.log(req.cookies)
+  res.send("check")
+})
+
+router.get("/delete", function(req, res){
+  res.clearCookie("age")
+  res.send("Clear ho gyi")
+})
+
+
+// Session Read Create Delete
+// router.get("/", function(req,res){
+//   req.session.ban = true
+//   res.render('index')
+// });
+// router.get("/checkban", function(req,res){
+//   console.log(req.session);
+//   if(req.session.ban === true){
+//     res.send("Check kiya hai console dekho")
+//   }
+//   else{
+//     res.send("Remove Ban")
+//   }
+// })
+
+// router.get("/removeban", function(req,res){
+//   req.session.destroy(function(err){
+//     if(err) throw err;
+//     res.send("Remove Banned")
+//   })
+// })
+ 
 // USERCREATE IN DATABASE 👇
 // router.get("/create", async function(req, res){
 //   const usercreated = await userModel.create({
@@ -17,7 +51,7 @@ router.get("/", function(req,res){
 //   res.send(usercreated)
 // })
 
-// READING ALL USERS 👇
+// SEE ALL USERS 👇
 router.get("/allusers", async function(req,res){
   const allUsers = await userModel.find(); // IF YOU SEE ALL USERS USE find() function
   // const allUsers = await userModel.findOne({username: "dharam1"}); // IF YOU SEE ONLY ONE USER USE findOne() function
