22f98e160a19d3bc1b5b57c43e228ef7c06f25a6
dharam-in <dharamrajyadav6312@gmail.com>
2024-03-08 23:04:55 +0530

    Data show on frontend

--- mern/src/Components/Card.js
+++ mern/src/Components/Card.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import Btn from './Btn'
 
-const Card = () => {
-  var img = "https://media.istockphoto.com/id/612759522/photo/fake-junk-food-on-a-wooden-table.jpg?s=612x612&w=0&k=20&c=zI-m6WyTpm-imNeQLoASndk5VpAETKebPpZomSmgnas="
+const Card = ({img, heading, desc, options}) => {
+  // var img = "https://media.istockphoto.com/id/612759522/photo/fake-junk-food-on-a-wooden-table.jpg?s=612x612&w=0&k=20&c=zI-m6WyTpm-imNeQLoASndk5VpAETKebPpZomSmgnas="
+
+  console.log(options)
+  const keyoption = Object.keys(options[0])
+  // console.log(keyoption)
+
   return (
     <>
         <div className="border p-4 rounded-md">
             <div className="imgdiv">
-                <img src={img} className='w-full' alt="image" />
+                <img src={img} className='w-full h-[250px] object-cover' alt="image" />
             </div>
             <div className="con">
-                <h3>Heading</h3>
-                <p className='mb-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro molestias fuga error ipsa eveniet! Labore, possimus animi. Sapiente, temporibus inventore.</p>
-                <div className="flex mb-3 gap-3">
+                <h3>{heading}</h3>
+                <p className='mb-3'>{desc}</p>
+                <div className="flex mb-3 gap-3 items-center">
                   <select name="" className='py-2 px-3'>
                     {
                       Array.from(Array(6), (e,i) => {
@@ -24,9 +29,16 @@ const Card = () => {
                   </select>
 
                   <select name="" className='py-2 px-3'>
-                    <option value="half">Half</option>
-                    <option value="full">Full</option>
+                    {
+                      keyoption.map((data) => {
+                        return(
+                          <option key={data} value={data}>{data}</option>
+                        )
+                      })
+                    }
                   </select>
+
+                  <p>Price:- </p>
                 </div>
                 <Btn BTNDATA={"Buy"} Route={"/"}/>
             </div>
--- mern/src/Pages/Home.js
+++ mern/src/Pages/Home.js
@@ -1,19 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../Components/Card'
 import Banner from '../Components/Banner'
 
 const Home = () => {
+
+  const [foodCate, setFoodCate] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async () => {
+    const response = await fetch("http://localhost:5000/api/fooddata", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    const data = await response.json()
+    // console.log(data)
+    setFoodItem(data[0])
+    setFoodCate(data[1])
+  }
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
   return (
     <>
 
       {/* Banner */}
       <Banner/>
 
-      <div className="grid grid-cols-4 gap-4">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <div>
+        {
+          foodCate != [] 
+          ? foodCate.map((data) => {
+            return(
+              <div key={data._id} className='mt-5'>
+                <h3 className='text-2xl font-semibold'>{data.CategoryName}</h3>
+
+                <hr className='mb-3 mt-5 bg-[#ff4121] h-[2px]'/>
+
+                <div className="grid sm:grid-cols-2 md:grid-cols-4 grid-cols-1 gap-4">
+                  {
+                    foodItem != [] 
+                    ? foodItem.filter((item) => item.CategoryName === data.CategoryName)
+                    .map(filterItems => {
+                      return(
+                        <div key={filterItems._id}>
+                          <Card img={filterItems.img} heading={filterItems.name} desc={filterItems.description} options={filterItems.options}/>
+                        </div>
+                      )
+                    })
+                    : ""
+
+                  }
+                </div>
+              </div>
+            )
+          }) 
+          : <div>hello</div>
+        }
       </div>
     </>
   )
